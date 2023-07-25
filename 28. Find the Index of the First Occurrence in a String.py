502ae8e89661037e1bb8b83dd19b74e3231655a0
dke <dkibui@outlook.com>
2023-07-25 22:56:26 +0300

    Added  28. Find the Index of the First Occurrence in a String.py

---
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,8 @@
+def strStr(haystack, needle):
+    for i in range(len(haystack)):
+        k = i + len(needle)
+        if k > len(haystack):
+            break
+        if haystack[i:k] == needle:
+            return i
+    return -1

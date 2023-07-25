7270aa999b0d74a627286eb4614d74926e6b7f33
dke <dkibui@outlook.com>
2023-07-25 20:44:54 +0300

    Added number-of-ways-to-climb-stairs.py

---
+++ number-of-ways-to-climb-stairs.py
@@ -0,0 +1,11 @@
+def count_num_paths_stairs(n):
+    if n <= 1:
+        return 1
+    pre_path = 1
+    cur_path = 1
+    for i in range(2, n + 1):
+        one_step = cur_path
+        two_steps = pre_path
+        pre_path = cur_path
+        cur_path = one_step + two_steps
+    return cur_path
